090ecdec7d66940c48698a33f3d13feeafa24813
Alecsander Castro <alec_xlive2@hotmail.com>
2023-08-29 10:14:35 -0300

    testing updante ProductDescription

--- src/pages-sections/product-details/ProductDescription.tsx
+++ src/pages-sections/product-details/ProductDescription.tsx
@@ -4,14 +4,20 @@ import { H3 } from "components/Typography";
 import Product from "models/Product.model";
 
 // ======================================================
-type ProductDescriptionProps = { product: Product };
+type ProductDescriptionProps = { product: Product | null };
 // ======================================================
 
 const ProductDescription: FC<ProductDescriptionProps> = ({ product }) => {
-  const { Name } = product;
+  if (!product) {
+    // Se product for nulo, você pode renderizar uma mensagem alternativa ou retornar null
+    return <Box>No product data available.</Box>;
+  }
+
+  // Se product não for nulo, você pode continuar com a renderização
+  const { ShortDescription } = product;
   return (
     <Box>
-      <Box>{Name}</Box>
+      <Box>{ShortDescription}</Box>
     </Box>
   );
 };
