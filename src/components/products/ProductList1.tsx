5838fb09ae649c2c665dff882f7102b11b16016e
Alecsander Castro <39681040+Alecsander-Castro@users.noreply.github.com>
2023-08-30 10:49:19 -0300

    change parameters name

--- src/components/products/ProductList1.tsx
+++ src/components/products/ProductList1.tsx
@@ -16,21 +16,19 @@ const ProductList1: FC<ProductListProps> = ({ products }) => {
         {products.map((item: Product) => (
           <Grid item lg={4} sm={6} xs={12} key={item.id}>
             <ProductCard1
-              id={item.id}
+              id={item.ProductId}
               URLKey={item.URLKey}
-              title={item.title}
-              price={item.price}
-              rating={item.rating}
-              imgUrl={item.thumbnail}
-              discount={item.discount}
+              title={item.Name}
+              price={item.SpecialPrice}
+              imgUrl={item.SmallImageUrl}
               ShortDescription={item.ShortDescription}
             />
           </Grid>
         ))}
       </Grid>
 
-      <FlexBetween flexWrap="wrap" mt={4}>
-        <Span color="grey.600">Showing 1-9 of 1.3k Products</Span>
+      <FlexBetween justifyContent={"end"} flexWrap="wrap" mt={4}>
+        {/* <Span color="grey.600">Showing 1-9 of 1.3k Products</Span> */}
         <Pagination count={10} variant="outlined" color="primary" />
       </FlexBetween>
     </Fragment>
